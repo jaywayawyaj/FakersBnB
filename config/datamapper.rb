a864f473d6cc283d35e6a870f2e8fe36e516b839
Jethro Whaley <jethro.whaley@gmail.com>
2019-01-11 10:05:21 +0000

    RACK_ENBV incorporated - rake should be funstioning correctly

--- config/datamapper.rb
+++ config/datamapper.rb
@@ -2,6 +2,6 @@ require './lib/user'
 require_relative '../lib/property'
 require 'data_mapper'
 
-DataMapper.setup(:default, "postgres://localhost/fakers_bnb_development")
+DataMapper.setup(:default, "postgres://localhost/fakers_bnb_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_migrate!
